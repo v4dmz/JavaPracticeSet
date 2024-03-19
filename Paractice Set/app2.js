daac1236a76a61aed616c66660b21439f3963afa
v4dmz <143401420+v4dmz@users.noreply.github.com>
2024-03-19 03:07:57 -0700

    JavaPracticeSet

---
+++ Paractice Set/app2.js
@@ -0,0 +1,33 @@
+// //Const : Const (constant) in programming is a keyword that defines a variable or pointer as unchangeable.
+//  const pi = 3.14159265 
+// let radius = 4;
+// // pi = 2;
+// console.log(pi * radius * radius);
+
+
+
+// pi * r
+
+
+
+
+// const myName = 'Ahmed';
+// document.querySelector('#name-display').
+// innerText = myName;
+
+
+const myName = 'Ahmed';
+document.querySelector('#name-display').
+innerText = myName;
+
+
+const Gstpercentage = 18;
+let cartValue = (15599-(10/100 * 15599))- 272 + Gstpercentage;
+console.log(cartValue);
+
+
+
+console.log(eval('2+3+5*7-9'));
+
+
+
